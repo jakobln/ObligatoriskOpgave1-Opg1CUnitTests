48fa7507cd7ce271f2252eb787c645ec2cba75c8
jakobln <62882745+jakobln@users.noreply.github.com>
2021-10-04 14:20:36 +0200

    Add files via upload

---
+++ UnitTestsOblOpg/FootballPlayer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace UnitTestsOblOpg
+{
+    public class FootballPlayer
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _shirtnumber;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name 
+        {
+            get { return _name; }
+            set 
+            {
+                if (value == null) throw new ArgumentNullException("Name is null");
+                if (value.Length < 4) throw new ArgumentException("Name must be 4 letters long");
+                _name = value;
+            }
+        }
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value < 1) throw new ArgumentOutOfRangeException("price", value, "price must be positive");
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get { return _shirtnumber; }
+            set
+            {
+                if (value < 1) throw new ArgumentOutOfRangeException("ShirtNumber", value, "shirtnumber cannot be lower than 1");
+                if (value > 100) throw new ArgumentOutOfRangeException("ShirtNumber", value, "shirtnumber cannot be higher than 100");
+                _shirtnumber = value;
+            }
+        }
+
+        public FootballPlayer(int id, string name, int price, int shirtnumber)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtnumber;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ UnitTestsOblOpgTests1/FootballPlayerTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestsOblOpg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsOblOpg.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        private FootballPlayer _player;
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            _player = new FootballPlayer(1, "Anders", 100, 100);
+        }
+
+        /*[TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }*/
+
+        [TestMethod]
+        public void TestNameNull()
+        {
+            Assert.AreEqual("Anders", _player.Name);
+            Assert.ThrowsException<ArgumentNullException>(() => _player.Name = null);
+            Assert.AreEqual("Anders", _player.Name);
+        }
+
+        [TestMethod]
+        public void TestNameLength()
+        {
+            Assert.AreEqual("Anders", _player.Name);
+            Assert.ThrowsException<ArgumentException>(() => _player.Name = "Jon");
+            Assert.AreEqual("Anders", _player.Name);
+        }
+
+        [TestMethod]
+        public void TestPrice()
+        {
+            Assert.AreEqual(100, _player.Price, 0);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _player.Price = 0);
+            Assert.AreEqual(100, _player.Price, 0);
+        }
+
+        [TestMethod]
+        public void TestShirtNumberTooHigh()
+        {
+            Assert.AreEqual(100, _player.ShirtNumber);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _player.ShirtNumber = 101);
+            Assert.AreEqual(100, _player.ShirtNumber);
+        }
+
+        [TestMethod]
+        public void TestShirtNumberTooLow()
+        {
+            _player.ShirtNumber = 1;
+            Assert.AreEqual(1, _player.ShirtNumber);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _player.ShirtNumber = 0);
+            Assert.AreEqual(1, _player.ShirtNumber);
+        }
+    }
+}
